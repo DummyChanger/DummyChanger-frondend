75e5be0038c92a316fa0d30669d2de27b57b9e61
lakshanamal <lakshanamal100@gmail.com>
2021-08-04 11:37:02 +0530

    sleve button added

--- src/Dummy/DummyPanel.js
+++ src/Dummy/DummyPanel.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function DummyPanel() {
-  return <div className="dummy-panel">hello</div>;
+  return (
+    <div className="dummy-panel text-light p-4">
+      <div className="sleve">
+        <h4>Sleve</h4>
+        <div>
+          <button type="button" className="btn btn-light">
+            Short Sleve
+          </button>
+          <button type="button" className="btn btn-light ms-3">
+            Long Sleve
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DummyPanel;
